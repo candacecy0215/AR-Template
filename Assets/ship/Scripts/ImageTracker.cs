92d042f523c8e43724e48375c06f8c7db5b28461
candacecy0215 <146746922+candacecy0215@users.noreply.github.com>
2025-02-04 12:14:54 +0000

    update

---
+++ Assets/ship/Scripts/ImageTracker.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+public class ImageTracker : MonoBehaviour
+{
+    [SerializeField]
+ARTrackedImageManager m_TrackedImageManager;
+
+public GameObject shipPrefab;
+
+void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
+
+void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+{
+    foreach (var newImage in eventArgs.added)
+    {
+        // Handle added event
+
+        GameObject newObject = GameObject.Instantiate(shipPrefab);
+        newObject.transform.SetParent(newImage.transform, false);
+    }
+
+    foreach (var updatedImage in eventArgs.updated)
+    {
+        // Handle updated event
+    }
+
+    foreach (var removedImage in eventArgs.removed)
+    {
+        // Handle removed event
+    }
+}
+}
