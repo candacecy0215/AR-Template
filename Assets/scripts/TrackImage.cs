928e867d2f5dde166b268ede5a500e5eeb416293
candacecy0215 <146746922+candacecy0215@users.noreply.github.com>
2025-04-08 12:36:54 +0100

    UPDDATE

---
+++ Assets/scripts/TrackImage.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation; // include xr library
+
+public class TrackImage : MonoBehaviour
+{
+    [SerializeField]
+    ARTrackedImageManager m_TrackedImageManager;
+    public GameObject bellPrefab; //Prefab you want to appear on marker image
+    public GameObject giftboxPrefab; //Prefab you want to appear on marker image
+    public GameObject wordsPrefab; //Prefab you want to appear on marker image
+
+    void Start()
+    {
+        // Disable screen dimming
+      //  Screen.sleepTimeout = SleepTimeout.NeverSleep;
+Debug.Log("start");
+    
+    }
+
+    void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
+
+    void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
+
+    void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
+    {
+       // AudioSource source = GetComponent<AudioSource>();
+
+        // When the camera picks up a new image marker Unity adds a game object to it called newImage, this will stick to maker.
+        foreach (ARTrackedImage newImage in eventArgs.added)
+        {
+
+         
+
+            if(newImage.referenceImage.name == "IMG_7476") {
+                // Create new copy of your prefab
+                GameObject newObject = GameObject.Instantiate(bellPrefab);
+                // parent prefab to the newImage so that they stick together.
+                newObject.transform.SetParent(newImage.transform, false);
+            }else if(newImage.referenceImage.name == "IMG_7740")
+            {
+                GameObject newObject = GameObject.Instantiate(giftboxPrefab);
+                newObject.transform.SetParent(newImage.transform, false);
+            }
+            else if(newImage.referenceImage.name == "map")
+            {
+                GameObject newObject = GameObject.Instantiate(wordsPrefab);
+                newObject.transform.SetParent(newImage.transform, false);
+            }
+            //source.Play();
+        }
+    }
+}
