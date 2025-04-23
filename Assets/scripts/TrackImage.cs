1592f62df18b2caf5bc0d693a007fbf83743ac23
candacecy0215 <146746922+candacecy0215@users.noreply.github.com>
2025-04-23 20:04:56 +0100

    update

--- Assets/scripts/TrackImage.cs
+++ Assets/scripts/TrackImage.cs
@@ -23,9 +23,11 @@ Debug.Log("start");
 
     void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
 
+    
+
     void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
-       // AudioSource source = GetComponent<AudioSource>();
+    //    AudioSource source = GetComponent<AudioSource>();
 
         // When the camera picks up a new image marker Unity adds a game object to it called newImage, this will stick to maker.
         foreach (ARTrackedImage newImage in eventArgs.added)
@@ -48,7 +50,7 @@ Debug.Log("start");
                 GameObject newObject = GameObject.Instantiate(wordsPrefab);
                 newObject.transform.SetParent(newImage.transform, false);
             }
-            //source.Play();
+            // source.Play();
         }
     }
 }
