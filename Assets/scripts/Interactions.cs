26789e14345e65302c352424e0acc880457b2ef1
candacecy0215 <146746922+candacecy0215@users.noreply.github.com>
2025-04-28 16:49:41 +0100

    update

--- Assets/scripts/Interactions.cs
+++ Assets/scripts/Interactions.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class Interactions : MonoBehaviour
 {
-      bool annotationVisible = false; //current visibility of ship
-    public GameObject annotation; // annotation object
+    bool annotationVisible = false; //current visibility of ship
+    public GameObject Giftbox; // annotation object
+
+
+
 
     public void selected()
     {
 
-        Debug.Log("show annotation");
+        Instantiate(Giftbox, new Vector3(0,7,0), Quaternion.identity);
+   
+
+/*
+        Debug.Log("falling giftbox");
         //toggle visibility of the annotation
         if (annotationVisible)
         {
-            annotation.SetActive(false);
-            annotationVisible = false;
+          //  annotation.SetActive(false);
+          // annotationVisible = false;
         }else {
-            annotation.SetActive(true);
-            annotationVisible = true;
+           // annotation.SetActive(true);
+           // annotationVisible = true;
         }
-
+*/
     }
 }
